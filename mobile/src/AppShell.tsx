745098f13bec65690efce9198a9116fce28e7055
abdelhadiLRS <abdelhadilarras.kdp@gmail.com>
2026-08-27 12:53:41 +0100

    Add mobile app shell foundation

---
+++ mobile/src/AppShell.tsx
@@ -0,0 +1,3 @@
+import React from 'react'
+import { SafeAreaView, Text, View } from 'react-native'
+export default function AppShell(){return <SafeAreaView><View><Text>JUBA LISAN</Text></View></SafeAreaView>}
