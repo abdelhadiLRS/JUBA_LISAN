c17bd502542c0a71f4d61af5eb09a0d6ab36eb03
abdelhadiLRS <abdelhadilarras.kdp@gmail.com>
2026-09-23 14:39:09 +0100

    style: refine separator with shared visual system

--- frontend/src/components/ui/separator.tsx
+++ frontend/src/components/ui/separator.tsx
@@ -1,20 +1,15 @@
 'use client'
 
 import { Separator as SeparatorPrimitive } from '@base-ui/react/separator'
-
 import { cn } from '@/lib/utils'
 
-function Separator({
-  className,
-  orientation = 'horizontal',
-  ...props
-}: SeparatorPrimitive.Props) {
+function Separator({ className, orientation = 'horizontal', ...props }: SeparatorPrimitive.Props) {
   return (
     <SeparatorPrimitive
       data-slot="separator"
       orientation={orientation}
       className={cn(
-        'shrink-0 bg-[var(--juba-border-soft)] data-horizontal:h-px data-horizontal:w-full data-vertical:w-px data-vertical:self-stretch',
+        'shrink-0 bg-[var(--juba-border)] data-horizontal:h-0.5 data-horizontal:w-full data-vertical:w-0.5 data-vertical:self-stretch',
         className
       )}
       {...props}
