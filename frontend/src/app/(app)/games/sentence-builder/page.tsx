fcf8c07aaaa7000b5f775a0d12e513af34528162
abdelhadiLRS <abdelhadilarras.kdp@gmail.com>
2026-09-25 02:27:58 +0100

    feat(games): add sentence builder route

---
+++ frontend/src/app/(app)/games/sentence-builder/page.tsx
@@ -0,0 +1,8 @@
+'use client'
+
+import '../games.css'
+import { InteractiveGamePage } from '@/components/games/InteractiveGamePage'
+
+export default function SentenceBuilderGamePage() {
+  return <InteractiveGamePage mode="sentence_builder" />
+}
